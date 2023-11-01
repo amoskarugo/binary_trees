b6f45cd82ff2871a0c32c878f58596e37b09a6b2
amoskarugo <karugoamos@gmail.com>
2023-11-01 16:08:47 +0000

    postorder traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_inorder - function that goes through a binary
+ *binary_tree_postorder - function that goes through a binary
  *                      tree using inorder traversal
  *@tree: a pointer to the root node of the tree to delete.
  *@func: a pointer to a function to call for each node.
@@ -17,5 +17,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	func(tree->n);
 }
