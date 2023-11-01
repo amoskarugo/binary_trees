13e5ff831cadc85e7576220f16482a113dd2964b
amoskarugo <karugoamos@gmail.com>
2023-11-01 14:24:34 +0000

    insert rigth

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_insert_rigth - a function that inserts a right-child on a node.
+ *binary_tree_insert_right - a function that inserts a right-child on a node.
  *@parent: pointer to the parent of the node to insert into.
  *@value: value to insert to the node.
  *Return: pointer to the node inserted or NULL on failure.
@@ -10,6 +10,7 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+
 	binary_tree_t *newnode, *temp;
 
 	if (parent == NULL)
@@ -17,7 +18,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	if(!newnode)
+	if (!newnode)
 		return (NULL);
 
 	newnode->n = value;
