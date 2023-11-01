1b13ff2f16dd71abe57f6ffc817e7ea52ef36f59
amoskarugo <karugoamos@gmail.com>
2023-11-01 09:26:24 +0000

    create node

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 
 
@@ -16,10 +17,10 @@
  */
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -27,4 +28,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif /* BINARY_TREES_H */
